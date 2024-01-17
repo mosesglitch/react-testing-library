3b266e551a3f15f097455421c11cf3df1727b5b2
mosesglitch <mosesgachuru15@gmail.com>
2024-01-17 13:18:30 +0300

    add email test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import { validator } from "validator";
 
 function App() {
+  const [signUpInput, setSignUpInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setSignUpInput({
+      ...signUpInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!signUpInput.email.match(validRegex)) {
+      return setError("the email you input is invalid");
+    }
+  };
+  console.log(signUpInput);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container my-5">
+      <form>
+        <div className="mb-5">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={signUpInput.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="Password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="Password"
+            name="password"
+            className="form-control"
+            value={signUpInput.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="confirm password" className="form-label">
+            Confirm password
+          </label>
+          <input
+            type="password"
+            id="confirm password"
+            name="confirmPassword"
+            className="form-control"
+            value={signUpInput.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        {error && <p className="text-danger">{error}</p>}
+        <button
+          type="submit"
+          name="submit"
+          className="btn btn-primary"
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,64 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+import App from "./App";
+//Always testhow the software is meant to be used, not how it is implemented
+// test block- You can have manya ssertions and if one test fails all the tests fails.
+// test("renders learn react link", () => {
+//   // render component and children we want to test to the virtual dom
+//   render(<App />);
+//   // finding the elements
+//   const linkElement = screen.getByText(/learn react/i); //gets anchor tag
+
+//   // an assertion
+//   expect(linkElement).toBeInTheDocument();
+// });
+test("iputs should be iitially empty", () => {
+  render(<App />); //pass in jsx form
+  const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />); //pass in jsx form
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  userEvent.type(emailInputElement, "selena@gmail.com");
+  expect(emailInputElement.value).toBe("selena@gmail.com");
+});
+
+test("should be able to type a password", () => {
+  render(<App />); //pass in jsx form
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "password!");
+  expect(passwordInputElement.value).toBe("password!");
+});
+
+test("should be able to type a password for confirmation", () => {
+  render(<App />); //pass in jsx form
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, "password!");
+  expect(confirmPasswordInputElement.value).toBe("password!");
+});
+
+test("should show email error message on invalid email", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailErrorElement = screen.queryByText(
+    /the email you input is invalid/i
+  ); //initially null
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
+
+  expect(emailErrorElement).not.toBeInTheDocument();
+
+  userEvent.type(emailInputElement, "selenagmacom");
+  userEvent.click(submitBtnElement);
+  const emailErrorElementAgain = screen.queryByText(/the email you/i);
+  expect(emailErrorElementAgain).toBeInTheDocument();
 });
