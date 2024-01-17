d73e0e8edeae315fd2065d86abfc2602c33c3fe2
mosesglitch <mosesgachuru15@gmail.com>
2024-01-17 13:57:04 +0300

    reorganize tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,43 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import App from "./App";
+
+beforeEach(() => {
+  render(<App />);
+});
+const typeIntoForm = ({ email, password, confirmPassword }) => {
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  if (email) {
+    userEvent.type(emailInputElement, email);
+  }
+  if (password) {
+    userEvent.type(passwordInputElement, password);
+  }
+  if (confirmPassword) {
+    userEvent.type(confirmPasswordInputElement, confirmPassword);
+  }
+  return {
+    emailInputElement,
+    passwordInputElement,
+    confirmPasswordInputElement,
+  };
+};
 //Always testhow the software is meant to be used, not how it is implemented
 // test block- You can have manya ssertions and if one test fails all the tests fails.
 // test("renders learn react link", () => {
 //   // render component and children we want to test to the virtual dom
-//   render(<App />);
+//
 //   // finding the elements
 //   const linkElement = screen.getByText(/learn react/i); //gets anchor tag
 
 //   // an assertion
 //   expect(linkElement).toBeInTheDocument();
 // });
-test("iputs should be iitially empty", () => {
-  render(<App />); //pass in jsx form
+test("inputs should be iitially empty", () => {
+  //pass in jsx form
   const emailInputElement = screen.getByRole("textbox");
   const passwordInputElement = screen.getByLabelText("Password");
   const confirmPasswordInputElement =
@@ -26,29 +50,37 @@ test("iputs should be iitially empty", () => {
 });
 
 test("should be able to type an email", () => {
-  render(<App />); //pass in jsx form
-  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-  userEvent.type(emailInputElement, "selena@gmail.com");
+  //pass in jsx form
+  // const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  // userEvent.type(emailInputElement, "selena@gmail.com");
+  const { emailInputElement } = typeIntoForm({
+    email: "selena@gmail.com",
+  });
   expect(emailInputElement.value).toBe("selena@gmail.com");
 });
 
 test("should be able to type a password", () => {
-  render(<App />); //pass in jsx form
-  const passwordInputElement = screen.getByLabelText("Password");
-  userEvent.type(passwordInputElement, "password!");
+  //pass in jsx form
+  // const passwordInputElement = screen.getByLabelText("Password");
+  // userEvent.type(passwordInputElement, "password!");
+  const { passwordInputElement } = typeIntoForm({
+    password: "password!",
+  });
   expect(passwordInputElement.value).toBe("password!");
 });
 
 test("should be able to type a password for confirmation", () => {
-  render(<App />); //pass in jsx form
-  const confirmPasswordInputElement =
-    screen.getByLabelText(/confirm password/i);
-  userEvent.type(confirmPasswordInputElement, "password!");
+  //pass in jsx form
+  // const confirmPasswordInputElement =
+  //   screen.getByLabelText(/confirm password/i);
+  // userEvent.type(confirmPasswordInputElement, "password!");
+  const { confirmPasswordInputElement } = typeIntoForm({
+    confirmPassword: "password!",
+  });
   expect(confirmPasswordInputElement.value).toBe("password!");
 });
 
 test("should show email error message on invalid email", () => {
-  render(<App />);
   const emailErrorElement = screen.queryByText(
     /the email you input is invalid/i
   ); //initially null
@@ -57,8 +89,8 @@ test("should show email error message on invalid email", () => {
 
   expect(emailErrorElement).not.toBeInTheDocument();
 
-  userEvent.type(emailInputElement, "selenagmacom");
-  userEvent.click(submitBtnElement);
-  const emailErrorElementAgain = screen.queryByText(/the email you/i);
-  expect(emailErrorElementAgain).toBeInTheDocument();
+  // userEvent.type(emailInputElement, "selenagmacom");
+  // userEvent.click(submitBtnElement);
+  // const emailErrorElementAgain = screen.queryByText(/the email you/i);
+  // expect(emailErrorElementAgain).toBeInTheDocument();
 });
